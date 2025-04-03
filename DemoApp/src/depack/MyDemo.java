7712751355a9f83a383a8b8dc8b8a3afafdc56fb
Me <Me@DESKTOP-4IC0CRN>
2025-04-03 16:18:29 +0530

    Second Version

---
+++ DemoApp/src/depack/MyDemo.java
@@ -0,0 +1,10 @@
+package depack;
+
+public class MyDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
--- DemoApp/src/depack/Welcome.java
+++ DemoApp/src/depack/Welcome.java
@@ -3,5 +3,7 @@ package depack;
 public class Welcome {
 public static void main(String[] args) {
 	System.out.println("hello");
+	System.out.println("hi");
+	
 }
 }
